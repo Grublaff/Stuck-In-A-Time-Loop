251a40575b965a17d719429b8219fcc5e1c5997d
Grubla <filip.aburg@gmail.com>
2021-04-16 00:51:31 +0200

    Add project files.

---
+++ Stuck In A Time Loop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Stuck_In_A_Time_Loop{
+    class Program{
+        static void Main(string[] args){
+            int runTimes = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < runTimes; i++){
+                if (i == 0){
+                    Console.WriteLine(i+1 + " Abracadabra");
+                }
+                else{
+                    Console.WriteLine(i+1 + " Abracadabra");
+                }  
+            }
+        }
+    }
+}
